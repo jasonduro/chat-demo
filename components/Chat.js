6d40fd58f2edc529037e0e6fffb5b3879d154293
Jason <product@jasonlamanque.com>
2023-05-26 21:05:15 +0200

    exercise 5.2 updates

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,43 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer!',
+        createdAt: new Date(),
+        user: {
+          _id: 2, 
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
   
  return (
-   <View style={[styles.container, {backgroundColor: color}]}>
-     <Text>Hello Screen2!</Text>
-   </View>
- );
+    <GiftedChat
+        messages={messages}
+        onSend={newMessages => onSend(newMessages)}
+        user={{
+          _id: 1,
+        }}
+      />
+  );
 }
 
 const styles = StyleSheet.create({
